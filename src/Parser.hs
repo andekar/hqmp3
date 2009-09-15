c37c0f7e93f0943bbe87bcdc610c2bf842a6570f
tobsan <tobsan@community.haskell.org>
2009-09-15 17:18:35 +0000

    Parser.hs: parsing most of the MPD protocol, some commands left and marked with TODO

--- src/Parser.hs
+++ src/Parser.hs
@@ -3,10 +3,10 @@ module Parser (parseCommand, toNumber) where
 import ApplicativeParsec
 import Data.Char
 
-data Commands = 
+data Command = 
     -- A command may be a list of commands
-      CList [Commands] 
-    -- Querying MPD's status
+      CList [Command] 
+    -- Querying status
     | ClearError
     | CurrentSong
     | Status
@@ -45,7 +45,7 @@ data Commands =
     | PlChangesPosId Int
     | Shuffle (Maybe (Int, Int))
     | Swap Int Int
-    | SwapID Int Int
+    | SwapId Int Int
     -- Stored playlists
     | Listplaylist String
     | ListplaylistInfo String
@@ -56,7 +56,7 @@ data Commands =
     | PlaylistDelete String Int
     | PlaylistMove String Int Int
     | Rename String String
-    | RM String
+    | Rm String
     | Save String
     -- The music database
     | Count -- todo
@@ -76,8 +76,8 @@ data Commands =
     | Password String
     | Ping
     -- Audio output
-    | DisableAudio
-    | EnableAudio
+    | DisableOutput
+    | EnableOutput
     | Output
     -- Reflection
     | Commands
@@ -87,15 +87,43 @@ data Commands =
     deriving Show
 
 parseCommand = parse pCommand "(unknown)"
--- TODO add all commands as in the datatype above
-pCommand =  pClose
-        <|> pPlay
-        <|> pClearError
-        <|> pCurrentSong
-        <|> pStatus
-        <|> pStatus
-        <|> pStats
-        <|> pList
+pCommand     =  pCommList
+            -- Querying status
+           <||> pClearError     <||> pCurrentSong   <||> pStatus
+           <||> pStats          <||> pIdle
+           -- Playback options
+           <||> pConsume        <||> pCrossfade     <||> pRandom
+           <||> pRepeat         <||> pSetvol        <||> pSingle
+           -- Controlling playback
+           <||> pNext           <||> pPrevious      <||> pStop
+           <||> pPause          <||> pPlay          <||> pPlayId
+           <||> pSeek           <||> pSeekId
+           -- The current playlist
+           <||> pClear          <||> pPlaylist      <||> pAdd
+           <||> pAddId          <||> pDelete        <||> pDeleteId
+           <||> pSwap           <||> pSwapId        <||> pShuffle
+           <||> pMove           <||> pMoveId        <||> pPlChanges
+           <||> pPlChangesPosId <||> pPlaylistId    <||> pPlaylistInfo
+           <||> pPlaylistSearch <||> pPlaylistFind
+           -- Stored playlists
+           <||> pLoad           <||> pSave          <||> pRename
+           <||> pRm             <||> pPlaylistClear <||> pPlaylistDelete
+           <||> pPlaylistMove   <||> pPlaylistMove  <||> pPlaylistAdd
+           <||> pListplaylists  <||> pListplaylist  <||> pListplaylistInfo
+           -- The music database
+           <||> pCount          <||> pFind          <||> pFindAdd
+           <||> pList           <||> pSearch        <||> pListAll
+           <||> pListAllInfo    <||> pLsInfo        <||> pUpdate
+           -- Stickers
+           <||> pSticker
+           -- Connection settings
+           <||> pClose          <||> pKill
+           <||> pPing           <||> pPassword
+           -- Audio output
+           <||> pDisableOutput  <||> pEnableOutput  <||> pOutput
+           -- Reflection
+           <||> pCommands       <||> pNotCommands   
+           <||> pTagTypes       <||> pURLHandlers
 
 --
 -- Parsing of commands below, in the same order they appear in 
@@ -103,16 +131,16 @@ pCommand =  pClose
 --
 
 -- List of commands
-pList = (string "command_list_begin" <|> string "command_list_ok_begin")
+pCommList = (string "command_list_begin" <|> string "command_list_ok_begin")
      *> (CList <$> many pCommand)
      <* string "command_list_end"
 
--- Querying MPD's status
+-- Querying status
 pClearError  = string "clearerror"  *> pure ClearError
 pCurrentSong = string "currentsong" *> pure CurrentSong
-pStatus      = string "status"      *> pure Status
-pStats       = string "stats"       *> pure Stats
-pIdle        = string "idle"        *> (pure Idle <$> optional (
+pStatus      = Status <$ string "status"
+pStats       = Stats  <$ string "stats"
+pIdle        = string "idle"        *> (Idle <$> optional (
                             string "database"        <|> string "update"
                         <|> string "stored_playlist" <|> string "playlist"
                         <|> string "player"          <|> string "mixer"
@@ -120,10 +148,10 @@ pIdle        = string "idle"        *> (pure Idle <$> optional (
 
 -- Playback options
 pConsume   = string "consume"   *> (Consume   <$> pBool)
-pCrossfade = string "crossfade" *> (Crossfade <$> pNumber)
+pCrossfade = string "crossfade" *> (Crossfade <$> pNum)
 pRandom    = string "random"    *> (Random    <$> pBool)
 pRepeat    = string "repeat"    *> (Repeat    <$> pBool)
-pSetvol    = string "setvol"    *> (Setvol    <$> pNumber)
+pSetvol    = string "setvol"    *> (Setvol    <$> pNum)
 pSingle    = string "single"    *> (Single    <$> pBool)
 
 -- Controlling playback 
@@ -131,29 +159,88 @@ pNext     = string "next"     *> pure Next
 pPrevious = string "previous" *> pure Previous
 pStop     = string "stop"     *> pure Stop
 pPause    = string "pause"    *> (Pause  <$> pBool)
-pPlay     = string "play"     *> (Play   <$> optional pNumber)
-pPlayId   = string "playid"   *> (PlayId <$> optional pNumber)
-pSeek     = string "seek"     *> (Seek   <$> pNumber <*> pNumber)
-pSeekId   = string "seekid"   *> (SeekId <$> pNumber <*> pNumber)
+pPlay     = string "play"     *> (Play   <$> optional pNum)
+pPlayId   = string "playid"   *> (PlayId <$> optional pNum)
+pSeek     = string "seek"     *> (Seek   <$> pNum <*> pNum)
+pSeekId   = string "seekid"   *> (SeekId <$> pNum <*> pNum)
 
 -- The current playlist
 pClear    = string "clear"    *> pure Clear
-pAdd      = string "add"      *> (Add      <$> many anyChar) --parse file name?
-pAddId    = string "addid"    *> (AddId    <$> many anyChar)
-pDelete   = string "delete"   *> (Delete   <$> pNumber)
-pDeleteId = string "deleteid" *> (DeleteId <$> pNumber)
+pPlaylist = string "playlist" *> pure Playlist
+pAdd      = string "add"      *> (Add      <$> pString) --parse file name?
+pAddId    = string "addid"    *> (AddId    <$> pString)
+pDelete   = string "delete"   *> (Delete   <$> pNum)
+pDeleteId = string "deleteid" *> (DeleteId <$> pNum)
+pSwap     = string "swap"     *> (Swap     <$> pNum <*> pNum)
+pSwapId   = string "swapid"   *> (SwapId   <$> pNum <*> pNum)
+pShuffle  = string "shuffle"  *> (Shuffle  <$> optional (pTuple pNum pNum))
+pMoveId   = string "moveid"   *> (MoveId   <$> pNum <*> pNum)
+pMove     = string "move"     *> (Move     <$> pEither pNum (pTuple pNum pNum)
+                                           <*> pNum)
+pPlChanges      = string "plchanges"      *> (PlChanges      <$> pNum)
+pPlChangesPosId = string "plchangesposid" *> (PlChangesPosId <$> pNum)
+pPlaylistId     = string "playlistid"     *> (PlaylistId     <$> optional pNum)
+pPlaylistInfo   = string "playlistinfo"   *> (PlaylistInfo   <$> optional 
+                                             (pEither pNum (pTuple pNum pNum)))
+pPlaylistSearch = undefined --TODO
+pPlaylistFind   = undefined --TODO
 
+-- Stored playlists
+pLoad           = string "load"   *> (Load   <$> pString)
+pSave           = string "save"   *> (Save   <$> pString)
+pRename         = string "rename" *> (Rename <$> pString <*> pString)
+pRm             = string "rm"     *> (Rm     <$> pString)
+pPlaylistClear  = string "playlistclear"  *> (PlaylistClear  <$> pString)
+pPlaylistDelete = string "playlistdelete" *> (PlaylistDelete <$> pString 
+                                                             <*> pNum)
+pPlaylistMove   = string "playlistmove"   *> (PlaylistMove   <$> pString
+                                                             <*> pNum <*> pNum)
+pPlaylistAdd    = string "playlistadd"    *> (PlaylistAdd    <$> pString 
+                                                             <*> pString)
+pListplaylists    = string "listplaylists"    *> pure Listplaylists
+pListplaylist     = string "listplaylist"     *> (Listplaylist     <$> pString)
+pListplaylistInfo = string "listplaylistinfo" *> (ListplaylistInfo <$> pString)
 
+-- The music database, mostly TODO here ;-)
+pCount       = undefined
+pFind        = undefined
+pFindAdd     = undefined
+pList        = undefined
+pSearch      = undefined
+pListAll     = string "listall"     *> (ListAll     <$> optional pString)
+pListAllInfo = string "listallinfo" *> (ListAllInfo <$> optional pString)
+pLsInfo      = string "lsinfo"      *> (LsInfo      <$> optional pString)
+pUpdate      = string "update"      *> (Update      <$> optional pString)
 
-pClose = string "close" *> pure Close
+-- Stickers, TODO here aswell ;-)
+pSticker = undefined
 
+-- Connection settings
+pClose    = string "close"    *> pure Close
+pKill     = string "kill"     *> pure Kill
+pPing     = string "ping"     *> pure Ping
+pPassword = string "password" *> (Password <$> pString)
+
+-- Audio output
+pDisableOutput = string "disableoutput" *> pure DisableOutput
+pEnableOutput  = string "enableoutput"  *> pure EnableOutput
+pOutput        = string "output"        *> pure Output
+
+-- Reflection
+pCommands    = string "commands"    *> pure Commands
+pNotCommands = string "notcommands" *> pure NotCommands
+pTagTypes    = string "tagtypes"    *> pure TagTypes
+pURLHandlers = string "urlhandlers" *> pure URLHandlers
 
 --
 -- Helper functions below
 --
 
-pBool = digit >>= return . (== '1')
-pNumber = many1 digit >>= return . toNumber
+pBool       = digit >>= return . (== '1')
+pNum        = many1 digit >>= return . toNumber
+pTuple f g  = f >>= \a -> g >>= \b -> return (a,b)
+pEither l r = (Left <$> l) <|> (Right <$> r)
+pString     = pString
 
 toNumber :: [Char] -> Int
 toNumber xs = foldl (\a x -> a*10 + (ord x - 48)) 0 xs
