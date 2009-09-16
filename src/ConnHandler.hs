f2fa317c4dc4a74e72aaf4ae991c3c3945021f5f
tobsan <tobsan@community.haskell.org>
2009-09-16 13:35:20 +0000

    ConnHandler.hs: began support for Command lists in network

--- src/ConnHandler.hs
+++ src/ConnHandler.hs
@@ -6,16 +6,17 @@ import Control.Concurrent
 import Data.Char
 import System.IO
 import Control.Monad
-import Data.List (isPrefixOf)
 import ChanHandler
+import Parser
 import Control.Monad.Trans
-import Control.Monad.Reader
 import Control.Exception
+import Data.Maybe
 
 startNetwork :: Show a => Chan (Command, Chan a) -> PortNumber -> IO ()
 startNetwork chan port = bracket (listenOn $ PortNumber $ fromIntegral port)
                          sClose (handler chan)
 
+-- Server listens for connections
 handler :: Show a => Chan (Command, Chan a) -> Socket -> IO ()
 handler chan s = forever $ do
     (h,_,_) <- accept s
@@ -24,26 +25,36 @@ handler chan s = forever $ do
                   clientHandler chan h}
     handler chan s
 
+-- Main handle type for clients
 clientHandler :: Show a => Chan (Command, Chan a) -> Handle -> IO ()
 clientHandler chan h = do
+    line <- hGetLine h
+    case parseCommand line of
+        Left err    -> undefined
+        Right comm  -> 
+            if (comm == CListBegin || comm == CListOkBegin)
+            then do
+                cs    <- getListCommands h [comm]
+                reply <- runCommands cs
+                hPutStrLn h (show $ fromJust reply) --fixme
+                hFlush h
+                clientHandler chan h
+            else do
+                reply <- runCommands [comm]
+                hPutStrLn h (show $ fromJust reply) --fixme
+                hFlush h
+                clientHandler chan h
+            
+-- Read a list of commands from the network, and parse these
+getListCommands :: Handle -> [Command] -> IO [Command]
+getListCommands h cs = do
     str <- hGetLine h
-    case parse str of
-        Left ack    -> do
-            hPutStrLn h ("ACK " ++ ack)
-            hFlush h
-            clientHandler chan h
-        Right Close -> hClose h
-        Right comm  -> do
-            retChan <- newChan
-            writeChan chan (comm,retChan)
-            reply <- readChan retChan
-            hPutStrLn h (show reply)
-            hFlush h
-            clientHandler chan h
+    case parseCommand str of
+        Right CListEnd -> return $ reverse (CListEnd : cs)
+        Right comm     -> getListCommands h (comm : cs)
+        Left ack       -> return $ cs
 
-parse :: String -> Either String Command
-parse s | "close" == (head xs) = Right Close
-        | "play" == (head xs)  = Right Play
-        | otherwise            = Left s
-    where xs = words s
-
+-- TODO
+-- Will create reply channels  
+runCommands :: [Command] -> IO (Maybe Ack)
+runCommands xs = undefined
