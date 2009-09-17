ca75a2a13beede548d69e969cd417b9cdd75b093
tobsan <tobsan@community.haskell.org>
2009-09-17 17:19:27 +0000

    ConnHandler looking like a dream

--- src/Network/ConnHandler.hs
+++ src/Network/ConnHandler.hs
@@ -1,60 +1,98 @@
 module Network.ConnHandler (startNetwork) where
 
-import Types
-import ChanHandler
-import Network.Parser
+import Types (Command (..), Reply (..), Ok (..) )
+import Network.Parser (parseCommand)
 
-import Network
-import System.IO
-import Control.Monad
-import Control.Exception
-import Control.Concurrent
+import Network (listenOn, accept, PortID (PortNumber), sClose, Socket)
+import System.IO (hPutStrLn, hFlush, hClose, Handle, hGetLine)
+import Control.Monad ((>>=), forever, forM_)
+import Control.Monad.Reader (runReaderT, ask, asks, lift, ReaderT)
+import Control.Exception (bracket)
+import Control.Concurrent (forkIO, Chan, writeChan, readChan, newChan)
+
+-- Convenient stuff
+type ClientHandler a = ReaderT Client IO a
+data Client = Client 
+        {   chan      :: Chan (Command, Chan Reply)
+          , replyChan :: Chan Reply
+          , h         :: Handle
+        }
 
 -- Starts the network, or exits if there was a problem
-startNetwork :: Show a => Chan (Command, Chan a) -> PortNumber -> IO ()
+startNetwork :: Chan (Command, Chan Reply) -> Int -> IO ()
 startNetwork chan port = bracket (listenOn $ PortNumber $ fromIntegral port)
                          sClose (handler chan)
 
 -- Server listens for connections
-handler :: Show a => Chan (Command, Chan a) -> Socket -> IO ()
+handler :: Chan (Command, Chan Reply) -> Socket -> IO ()
 handler chan s = forever $ do
-    (h,_,_) <- accept s
-    forkIO $ do { hPutStrLn h "OK HQmpd 0.1";
-                  hFlush h;
-                  clientHandler chan h}
+    (h,host,_) <- accept s
+    print $ host ++ " connected"
+    forkIO $ do 
+        hPutStrLn h "OK HQmpd 0.0"
+        hFlush h
+        reply <- newChan
+        runReaderT clientHandler (Client chan reply h)
     handler chan s
 
+
 -- Main handle type for clients
-clientHandler :: Show a => Chan (Command, Chan a) -> Handle -> IO ()
-clientHandler chan h = do
-    line <- hGetLine h
+clientHandler :: ClientHandler ()
+clientHandler = do
+    h  <- asks h
+    line <- lift $ hGetLine h
     case parseCommand line of
         Left err    -> undefined
-        Right comm  -> 
-            if (comm == CListBegin || comm == CListOkBegin)
-            then getListCommands h [comm] >>= runCommands >>= reply
-            else runCommands [comm] >>= reply
+        Right Close -> do
+            lift $ hPutStrLn h (show Ok)
+            lift $ hClose h
+        Right CListBegin -> do
+            list    <- getListCommands [CListBegin]
+            replies <- runCommands list False
+            forM_ replies reply
+        Right CListOkBegin -> do
+            list    <- getListCommands [CListOkBegin]
+            replies <- runCommands list True
+            forM_ replies reply
+        Right comm  -> runCommands [comm] False >>= mapM_ reply
   where
     reply response = do
-        hPutStrLn h $ show' response
-        hFlush h
-        clientHandler chan h
+        h <- asks h
+        lift $ hPutStrLn h $ show' response
+        lift $ hFlush h
+        clientHandler
             
 -- Read a list of commands from the network, and parse these
-getListCommands :: Handle -> [Command] -> IO [Command]
-getListCommands h cs = do
-    str <- hGetLine h
+getListCommands :: [Command] -> ClientHandler [Command]
+getListCommands cs = do
+    h   <- asks h
+    str <- lift $ hGetLine h
     case parseCommand str of
         Right CListEnd -> return $ reverse (CListEnd : cs)
-        Right comm     -> getListCommands h (comm : cs)
+        Right comm     -> getListCommands (comm : cs)
         Left ack       -> return $ cs
 
--- TODO
--- Will create reply channels  
-runCommands :: [Command] -> IO (Either Ok Ack)
-runCommands xs = undefined
-
+-- Sends commands to the server and listens for replies
+-- from the ChanHandler
+runCommands :: [Command] -> Bool -> ClientHandler [Reply]
+runCommands xs list = do
+    env <- ask
+    forM_ xs $ \x -> lift $ writeChan (chan env) (x, (replyChan env))
+    lift $ listenReply (length xs) (replyChan env) list
+  where
+    listenReply :: Int -> Chan Reply -> Bool -> IO [Reply]
+    listenReply 0 _ _ = return []
+    listenReply i chan list = do
+        val <- readChan chan
+        case val of
+            Left ack -> return [val]
+            Right _  -> do
+                vals <- listenReply (i-1) chan list
+                if list then return $ (Right ListOk):vals
+                        else return $ (Right Ok):vals
+        
 -- Show for either includes ugly "Right/Left"
+-- This one is WAAY happier
 show' :: (Show a, Show b) => Either a b -> String
 show' (Left a)  = show a
 show' (Right b) = show b
