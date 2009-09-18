f834f4ed0438a32efa1048aa1a0d547b985e5d7a
tobsan <tobsan@community.haskell.org>
2009-09-18 14:23:30 +0000

    ChanHandler: added some shell code

--- src/ChanHandler.hs
+++ src/ChanHandler.hs
@@ -3,7 +3,7 @@ module ChanHandler (server) where
 import Types
 import Control.Concurrent
 import Control.Concurrent.Chan
-import Control.Monad
+import Control.Monad.State
 import Control.Monad.Trans
 import Player
 
@@ -16,7 +16,13 @@ server = do
 
 checkPlayerChan :: Server a b ()
 checkPlayerChan = do
-    return ()
+    (pChan,rChan) <- gets playerChan
+    comm <- lift $ maybeGetChan pChan
+    case comm of
+        Nothing -> return ()
+        Just c  -> do
+            playerCommand c
+            return ()
 
 checkCommandChan :: Server a b ()
 checkCommandChan = do 
@@ -27,3 +33,7 @@ maybeGetChan :: Chan a -> IO (Maybe a)
 maybeGetChan c = isEmptyChan c >>= \e -> case e of
     True -> return Nothing
     _    -> readChan c >>= return . Just
+
+
+playerCommand :: Command -> Server a b ()
+playerCommand comm = undefined
