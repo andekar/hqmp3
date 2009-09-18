9cc9ba803368c3a8039371e2481e5e47ee97b4c0
tobsan <tobsan@community.haskell.org>
2009-09-18 14:24:32 +0000

    Database.hs: addDir working as one would expect now

--- src/Database.hs
+++ src/Database.hs
@@ -5,29 +5,34 @@ import qualified Data.Set as Set
 import qualified Data.Map as Map
 import Control.Monad
 import System.Directory
-import System.FilePath (takeExtension)
+import System.FilePath
 
--- Recursively add directories to the database
--- TODO change path to be relative to "root"
 addDir :: FilePath -> IO Database
-addDir dir = do
-    fs <- getDirectoryContents dir
-    let fs' = map (dir++) $ filter (\(x:xs) -> x /= '.') fs
-
-    subdirs <- filterM doesDirectoryExist fs' 
-    files   <- filterM doesFileExist fs'
-    subDB   <- mapM addDir $ map (++"/") subdirs
-
-    return $ Dir dir
-        (Set.fromList $ map mkSong $ filter isMedia files)
-        (Map.fromList $ map (\d@(Dir f _ _) -> (f,d)) subDB)
+addDir path = do
+    allFiles <- getDirectoryContents path
+    let allFiles' = map (\f -> path </> f) $ filter (\(x:xs) -> x /= '.') allFiles
+    (dirs,files) <- splitFiles allFiles'
+    subDirs       <- mapM addDir dirs
+    let subDirs'  = filter nonEmpty subDirs
+    return $ Dir path (Set.fromList $ map mkSong files) 
+                      (Map.fromList $ map (\d@(Dir f _ _) -> (f,d)) subDirs')
+  where
+    nonEmpty (Dir _ d f) | Set.null d && Map.null f = False 
+                         | otherwise                = True
+    
+    splitFiles :: [FilePath] -> IO ([FilePath],[FilePath])
+    splitFiles []             = return ([],[])
+    splitFiles (x:xs)         = do
+        dir <- doesDirectoryExist x
+        (dirs,files) <- splitFiles xs
+        if dir then return $ (x:dirs,files)
+               else if isMedia x then return $ (dirs,x:files)
+                                 else return $ (dirs,files)
 
 -- Checks if the a file is a music file
 isMedia :: FilePath -> Bool
 isMedia f = takeExtension f == ".mp3"
 
 -- Reads the file, and computes ID3 and such
-mkSong :: FilePath -> Song
-mkSong f = undefined
-
-
+mkSong :: FilePath -> String 
+mkSong = id
