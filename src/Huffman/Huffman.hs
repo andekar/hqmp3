e9dbfb63ba40688948a1c43f17f463baa24e97cc
andekar <andekar@community.haskell.org>
2009-09-25 05:48:45 +0000

    Huffman.hs: small improvements

--- src/Huffman/Huffman.hs
+++ src/Huffman/Huffman.hs
@@ -10,16 +10,10 @@ module Huffman ( analyze
 import qualified Data.ByteString as B
 import qualified Data.Map as M
 import Data.Word
-import Data.List
 import Data.PriorityQueue
 import Control.Monad
 import Control.Monad.ST
 import Data.Bits
-
---main :: FilePath -> IO (HuffTree (Word8,Int)) 
-main file = do
-    f <- B.readFile file
-    return $ create $ analyze f
 
 -- Can probably be speeded up (TODO)
 analyze :: B.ByteString -> [(Word8,Int)]
@@ -28,41 +22,41 @@ analyze b = M.toList $ B.foldr f M.empty b
     f :: Word8 -> M.Map Word8 Int -> M.Map Word8 Int
     f w m = M.insertWith (+) w 1 m
 
-data (Eq a, Num b) => HuffTree a b =
-    Leaf !b !a
-  | Node !b !(HuffTree a b) !(HuffTree a b)
-    deriving (Show,Read)
+data (Eq a) => HuffTree a =
+    Leaf !a
+  | Node !(HuffTree a) !(HuffTree a)
+    deriving (Show, Read)
 
-instance (Eq a, Num b) => Eq (HuffTree a b) where
-    Leaf b a     == Leaf b' a'      = b == b && a == a'
-    Node b t1 t2 == Node b' t1' t2' = b == b && t1 == t1' && t2 == t2'
+instance (Eq a) => Eq (HuffTree a) where
+    Leaf a     == Leaf a'      = a == a'
+    Node t1 t2 == Node t1' t2' = t1 == t1' && t2 == t2'
     _            == _               = False
 
-instance (Eq a, Ord b, Num b) => Ord (HuffTree a b) where
-    t1 <= t2 = getVal t1 <= getVal t2
+-- instance (Eq a) => Ord (HuffTree a) where
+--     t1 <= t2 = getVal t1 <= getVal t2
 
 -- create :: (Eq a, Num b, Ord b) => [(a,b)] -> HuffTree a b
 create xs = runST $ do
-    q <- newPriorityQueue id
-    mapM_ (\(e,i) -> enqueue q (Leaf i e)) xs
+    q <- newPriorityQueue fst
+    mapM_ (\(e,i) -> enqueue q (i, (Leaf e))) xs
     create' q
 
-create' :: (Eq a, Num b, Ord b) => PriorityQueue (ST s) (HuffTree a b)
-           -> (ST s (HuffTree a b))
+create' :: (Eq a) => PriorityQueue (ST s) (Int, HuffTree a)
+           -> (ST s (HuffTree a))
 create' p = do
     (Just x1) <- dequeue p
     x         <- dequeue p
     case x of
-        Nothing -> return x1
+        Nothing -> return $ snd x1
         Just x2 -> do
             enqueue p $ merge x1 x2
             create' p
   where
-    merge :: (Eq a, Num b) => HuffTree a b -> HuffTree a b -> HuffTree a b
-    merge x1 x2 = Node (getVal x1 + getVal x2) x1 x2
+    merge :: Eq a => (Int, HuffTree a) -> (Int, HuffTree a) -> (Int, HuffTree a)
+    merge (i, x1) (i', x2) = (i + i', Node x1 x2)
 
-getVal (Leaf w _)   = w
-getVal (Node w _ _) = w
+-- getVal (Leaf w _)   = w
+-- getVal (Node w _ _) = w
  
 -- Decodes a list of Words, using a Huffman tree
 -- 
@@ -73,22 +67,16 @@ getVal (Node w _ _) = w
 -- j is how many padding bits this stream has
 --
 -- Please do not change this code, checks are not checked
-decode :: (Num b, Eq a) =>
-     [Word8] -> HuffTree a b -> HuffTree a b -> Int -> Int -> [a]
+decode :: (Eq a) =>
+     [Word8] -> HuffTree a -> HuffTree a -> Int -> Int -> [a]
 
-decode xs t (Leaf _ v) i j = v : decode xs t t i j
+decode xs t (Leaf v) i j = v : decode xs t t i j
 decode [] _ _ i j   = if i == j && j == 0 then [] else error "error"
-decode (x:[]) t (Node _ t1 t2) i j
+decode (x:[]) t (Node t1 t2) i j
     | i+1 == j = [] 
     | otherwise = decode [x] t (tree x i t1 t2) (i - 1) j
-decode (x:xs) t (Node _ t1 t2) i j
+decode (x:xs) t (Node t1 t2) i j
     | i == 0 = decode xs t (tree x i t1 t2) 7 j
     | otherwise = decode (x:xs) t (tree x i t1 t2) (i - 1) j
 
-tree bit num t1 t2 = if testBit bit num then t2 else t1
-
-tree2Map :: (Num b, Num c, Ord a) => HuffTree a b -> c -> M.Map a c -> M.Map a c
-tree2Map (Leaf _ a) i  m    = M.insert a i m
-tree2Map (Node _ t1 t2) i m =
-    let m' = tree2Map t1 (i * 2) m
-    in tree2Map t2 (i * 2 + 1) m'
+tree bits num t1 t2 = if testBit bits num then t2 else t1
