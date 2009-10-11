5e53e063c7c5b5f0c5c3c42619dda525c4b53eac
tobsan <tobsan@community.haskell.org>
2009-10-11 18:00:03 +0000

    BitGet.hs - readN is sort of finished, we just need to understand it now...

--- src/Data/Binary/BitGet.hs
+++ src/Data/Binary/BitGet.hs
@@ -1,10 +1,11 @@
 module BitGet where
 
 import qualified Data.ByteString.Lazy as L
+import qualified Data.ByteString.Lazy.Internal as L
 import qualified Data.ByteString as S
 import Data.Word
 
-data S = S   S.ByteString -- The first lazy part of the bytestring
+data S = S   S.ByteString -- The first strict part of the bytestring
              L.ByteString -- The rest of the byteString
              Int        -- The bit we're standing at
 
@@ -15,10 +16,12 @@ runBitGet :: BitGet a -> S -> a
 runBitGet g s = case unGet g s of
     (a,_) -> a
 
+-- TODO make the type of BitGet an either, and implement fail
 instance Monad BitGet where
     return a = BitGet (\s -> (a,s))
     bg >>= f = BitGet $ \s -> case unGet bg s of
         (a,s') -> unGet (f a) s'
+    fail s = undefined
 
 instance Functor BitGet where
     f `fmap` bg = BitGet $ \s -> case unGet bg s of
@@ -36,7 +39,7 @@ skip :: Int -> BitGet ()
 skip i = readN i (const ())
 
 getAsWord8 :: Int -> BitGet Word8
-getAsWord8 i = readN i L.head
+getAsWord8 i = readN i S.head
 
 getAsWord16 :: Int -> BitGet Word16
 getAsWord16 i = undefined
@@ -48,17 +51,51 @@ lookAhead bg = do
     put s
     return a
 
--- Read N bits into a bytestring, and convert to some type
-readN :: Int -> (L.ByteString -> a) -> BitGet a
+-- Remember that we have to shift all read bytestrings left
+-- like... j bits
+readN :: Int -> (S.ByteString -> a) -> BitGet a
 readN i f = do
     (S sb lb j) <- get
-    case i < (S.length sb * 8 - (8-j)) of
-        True | i < (8-j)  -> undefined
-             | i == (8-j) -> undefined
-             | otherwise  -> undefined
-        False -> undefined 
+    if i < (8-j) then do
+        let sb' = S.take 1 sb
+        put $ S sb lb (i+j)
+        return (f sb') 
+        else if i == (8-j) then do
+            let (sb',sb'') = S.splitAt 1 sb
+            case S.null sb'' of
+                False -> put $ S sb'' lb 0
+                True  -> case lb of
+                    L.Empty             -> put $ S sb'' lb 0
+                    (L.Chunk sb''' lb') -> put $ S sb''' lb' 0
+            return (f sb')
+            else do
+                let i'  = i+j
+                    j'  = i' `mod` 8
+                    t   = if j' == 0 then 0 else 1
+                    i'' = i' `div` 8 + t
+                case S.length sb > i'' of
+                    True -> do
+                        let sb'  = S.take i'' sb
+                            sb'' = S.drop (i'' - t) sb
+                        return (f sb')
+                        -- seriously, this looks like crap...what was kolmodin
+                        -- thinking? splitAt? S.concat? L.toChunks? swamp...c?
+                    False -> case L.splitAt (fromIntegral i'') (sb `join` lb) of
+                        (consuming, rest) -> do 
+                        let now = S.concat . L.toChunks $ consuming
+                        put $ mkState (now,rest) j'
+                        if (S.length now < i'') 
+                            then fail "Hej"
+                            else return (f now)
+  where
+    mkState :: (S.ByteString, L.ByteString) -> Int -> S
+    mkState (_,(L.Chunk sb lb)) 0 = S sb lb 0
+    mkState (_,L.Empty) 0         = S S.empty L.Empty 0
+    mkState (sb,lb) j             = S sb' lb j
+        where sb' = S.drop (S.length sb - 1) sb
 
--- Join two bytestrings
 join :: S.ByteString -> L.ByteString -> L.ByteString
-join sb lb = undefined
-    
+join sb lb
+    | S.null sb = lb
+    | otherwise = L.Chunk sb lb
+
