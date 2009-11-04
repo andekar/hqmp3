b1949d8568afa960bd03335d52c75e7e038c00ae
andekar <andekar@community.haskell.org>
2009-11-04 14:42:08 +0000

    Test.hs: Added

---
+++ src/Codecs/Mp3/Test.hs
@@ -0,0 +1,24 @@
+module Test () where
+import BitGet
+import qualified Huffman as Huff
+-- import Data.Binary.Strict.BitGet
+import Data.Bits
+import Data.Word
+import Data.Maybe
+import qualified Data.ByteString.Lazy as L
+import qualified Data.ByteString as S
+import ID3
+import Debug.Trace
+import Control.Monad
+import Decoder
+import MP3Types
+import Unpack
+
+main = test "/home/tobsi/machinae_supremacy-cryosleep.mp3"
+
+test :: FilePath -> IO ()
+test f = do
+    file <- L.readFile f
+    print $ L.head file
+    let fs = unpackMp3 file
+    mapM_ print fs
