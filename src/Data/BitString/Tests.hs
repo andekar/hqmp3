c45ca22db0a43485aa0c05b506695246a7ee06e7
tobsan <tobsan@community.haskell.org>
2009-11-12 18:59:31 +0000

    Tests.hs: some handy stuff (might not need these later)

--- src/Data/BitString/Tests.hs
+++ src/Data/BitString/Tests.hs
@@ -105,19 +105,27 @@ prop_atLeastBS i bs = ((L.length bs * 8) >= (fromIntegral i))
                     == atLeastBS bs (fromIntegral i)
 
 -- TODO fix these, numbers are sort of reversed
+-- also, how does one do when begin and/or end are not 0?
 bisToList :: BitString -> [Int]
 bisToList bs = nums
-  where nums = btl bs
+  where 
+    nums = btl bs
 
-btl :: BitString -> [Int]
-btl Empty = []
-btl (Chunk lb begin end rest)
-    | L.null lb = bisToList rest
-    | begin+end == 8 && L.length lb == 1 = bisToList rest
-    | begin == 7 = f bit : (bisToList $ Chunk (L.tail lb) 0 end rest)
-    | otherwise  = f bit : (bisToList $ Chunk lb (begin+1) end rest)
+    btl :: BitString -> [Int]
+    btl Empty = []
+    btl (Chunk lb begin end rest)
+        | L.null lb = bisToList rest
+        | begin+end == 8 && L.length lb == 1 = bisToList rest
+        | begin == 7 = f bit : (bisToList $ Chunk (L.tail lb) 0 end rest)
+        | otherwise  = f bit : (bisToList $ Chunk lb (begin+1) end rest)
+      where
+        bit = testBit (L.head lb) (fi begin)
+        f True  = 1
+        f False = 0
+
+-- Recursive splitAt
+splitAtMany :: Int -> [a] -> [[a]]
+splitAtMany _ [] = []
+splitAtMany i xs = f : splitAtMany i b
   where
-    bit = testBit (L.head lb) (fi begin)
-    f True  = 1
-    f False = 0
-
+    (f,b) = List.splitAt i xs
