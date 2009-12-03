a6568db4c91039550693a814f42b643c27d57091
tobsan <tobsan@community.haskell.org>
2009-12-03 18:09:49 +0000

    Test.hs: working...

--- src/Codecs/Mp3/Test.hs
+++ src/Codecs/Mp3/Test.hs
@@ -32,25 +32,12 @@ test f = do
         PCM.writeSamples out left right
         putStrLn "# Oh yeah!"
   where
-    extract :: DChannel Double -> ([Double],[Double])
-    extract (DMono (_,cd1) (_,cd2))                   = undefined
-    extract (DStereo (_,cd1) (_,cd2) (_,cd3) (_,cd4)) = 
-        (fromChanData cd1 ++ fromChanData cd3,
-         fromChanData cd2 ++ fromChanData cd4)
+    extract :: DChannel [Double] -> ([Double],[Double])
+    extract chan = case chan of
+        Single _ _ _ g0 g1 -> (fromChanData $ mp3Data g0, fromChanData $ mp3Data g1)
+        Dual _ _ _ g0 g1 g2 g3 -> 
+            ( (fromChanData $ mp3Data g0) ++ (fromChanData $ mp3Data g2), 
+              (fromChanData $ mp3Data g1) ++ (fromChanData $ mp3Data g3) )
 
-    fromChanData :: ChannelData Double -> [Double]
+    fromChanData :: ChannelData [Double] -> [Double]
     fromChanData (ChannelData _ as) = as
-
-getLast :: DChannel Double -> Double
-getLast (DMono _ (_,cd2)) = case cd2 of
-    ChannelData _ as -> if not (null as) then last as else 0.0
-getLast (DStereo _ _ _ (_,cd4)) = case cd4 of
-    ChannelData _ as -> if not (null as) then last as else 0.0
-
-fun :: DChannel a -> Int
-fun m = case m of
-    (DMono   (g1,ch1) (g2,ch2) ) -> r ch1 + r ch2
-    (DStereo (g1,ch1) (g2,ch2) (g3,ch3) (g4,ch4)) -> r ch1 + r ch2 + 
-                                                     r ch3 + r ch4
-  where r (ChannelData sc list) = length list `seq` 1
-        r' (Scales l1 l2) = length l2 `seq` 1
