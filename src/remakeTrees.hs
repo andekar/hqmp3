f9879087ec052f4b849750ac597d8a9bafdb441f
tobsan <tobsan@community.haskell.org>
2009-12-10 17:26:15 +0000

    remakeTrees remade...sort of

--- src/remakeTrees.hs
+++ src/remakeTrees.hs
@@ -1,18 +1,34 @@
 import Codec.Compression.Huffman.Huffman
 import Data.List
 import Control.Monad
+import Control.Arrow
 import Data.Array.IArray
 import Tables
 
+type HuffEither = Either (Int,Int) (Array Int (Int,(Int,Int)))
+
+-- toArray :: [([Int],(Int,Int))] -> Array Int (Int, HuffEither)
+toArray xs 
+  = let (good,bad) = filterLength xs mean
+        lowhigh   = map (first (splitAt mean)) bad
+        grouped   = groupBy (\a b -> (fst . fst) a == (fst . fst) b) lowhigh
+        grouped'  = map (\as -> (fst $ fst $ head as, map (\((a,b),c) -> (b,c)) as)) grouped
+        grouped'' = map (second (Right . toArray' (0,0))) grouped'
+        allLists  = grouped'' ++ map (second Left) good
+    in grouped -- allLists -- toArray' (Left (0,0)) allLists
+
+  where
+    mean = (sum $ map (length . fst) xs) `div` (length xs)
+    filterLength xs l = partition (\a -> length (fst a) < l) xs
 
 
 -- Todo we must fix so that stuff get into the correct position
 -- as of now the smallest element would be at position zero
-toArray :: [([Int], (Int,Int))] -> Array Int (Int, (Int,Int))
-toArray xs = let (lists,l') = allLists xs
-                 l = 2^l' -1
-                 list = listArray (0,l)
-                        (insertEmpty l 0 (sorted lists))
+toArray' :: a -> [([Int], a)] -> Array Int (Int, a)
+toArray' zval xs = let (lists,l') = allLists xs
+                       l = 2^l' -1
+                       list = listArray (0,l)
+                              (insertEmpty l 0 zval (sorted lists))
              in list
     where sorted list = sortBy (\(x,_,_) (x',_,_) -> compare x x') (ls list)
           ls :: [([Int],a,b)] -> [(Int,a,b)]
@@ -23,17 +39,17 @@ toArray xs = let (lists,l') = allLists xs
 
 longest lists = foldl (\x (y,_) -> max x (length y)) 0 lists
 
-insertEmpty :: Int -> Int -> [(Int, Int, (Int,Int))] -> [(Int, (Int,Int))]
-insertEmpty len curr []
-    | curr < len = replicate (len - curr) (0,(0,0))
+insertEmpty :: Int -> Int -> a -> [(Int, Int, a)] -> [(Int, a)]
+insertEmpty len curr zval []
+    | curr < len = replicate (len - curr) (0,zval)
     | otherwise = []
-insertEmpty len curr ((x,m,a):xs)
-    | curr < x = replicate (x-curr) (0,(0,0))
-            ++ [(m,a)] ++ insertEmpty len (x+1) xs
-    | x == curr = (m,a):insertEmpty len (curr + 1) xs
+insertEmpty len curr zval ((x,m,a):xs)
+    | curr < x = replicate (x-curr) (0,zval)
+            ++ [(m,a)] ++ insertEmpty len (x+1) zval xs
+    | x == curr = (m,a):insertEmpty len (curr + 1) zval xs
     | otherwise = error "should maybe not happen"
 
-allLists :: [([Int],(Int,Int))] -> ([([Int],Int,(Int,Int))], Int)
+allLists :: [([Int],a)] -> ([([Int],Int,a)], Int)
 allLists lists@((x,a):xs) = ((concat $ map (all longest) lists), longest)
     where longest = foldl (\x (y,_) -> max x (length y)) 0 lists
           all n (x, a)
@@ -43,7 +59,6 @@ allLists lists@((x,a):xs) = ((concat $ map (all longest) lists), longest)
 
 allCom :: Int -> [Int] -> [[Int]]
 allCom long x = map (x ++) (perm long)
-
 
 permute :: [a] -> [[a]]
 permute str = rotate str len len
