acbacde2fc1a6ed4bff8e65d4f1d8382a0e1a1c2
tobsan <tobsan@community.haskell.org>
2009-12-16 10:16:38 +0000

    remakeTrees fixed AGAIN

--- src/remakeTrees.hs
+++ src/remakeTrees.hs
@@ -17,7 +17,7 @@ main = do
     putStrLn "-- XY Tables below..."
     mapM_ output (zip ([1..3] ++ [5..13] ++ [15,16,24]) createTreesXY)
   where
-    output (num, tree) = putStrLn $ "tree"++(show num)++ " = " ++ (show tree)
+    output (num, tree) = putStrLn $ "table"++(show num)++ " = " ++ (show tree)
 
 -- Comments represent the number as used in ISO-11172-3
 -- The first five (1-6) are shallow, whereas the other are deep
