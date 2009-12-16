164e81bcd1681d986c43eb7a74d581161c19d02a
tobsan <tobsan@community.haskell.org>
2009-12-16 14:23:57 +0000

    remakeTrees somewhat tweaked

--- src/remakeTrees.hs
+++ src/remakeTrees.hs
@@ -21,6 +21,7 @@ main = do
 
 -- Comments represent the number as used in ISO-11172-3
 -- The first five (1-6) are shallow, whereas the other are deep
+-- All tables with longest codeword <= 11 are not cut.
 createTreesXY :: [MP3Huffman (Int,Int)]
 createTreesXY =
     [ Left $ toArray' tableHuffR00  -- table 1
@@ -28,59 +29,44 @@ createTreesXY =
     , Left $ toArray' tableHuffR02  -- table 3
     , Left $ toArray' tableHuffR03  -- table 5
     , Left $ toArray' tableHuffR04  -- table 6
-    , Right $ toArray tableHuffR05  -- table 7
-    , Right $ toArray tableHuffR06  -- table 8
-    , Right $ toArray tableHuffR07  -- table 9
-    , Right $ toArray tableHuffR08  -- table 10
-    , Right $ toArray tableHuffR09  -- table 11
-    , Right $ toArray tableHuffR10  -- table 12
-    , Right $ toArray tableHuffR11  -- table 13
-    , Right $ toArray tableHuffR12  -- table 15
-    , Right $ toArray tableHuffR13  -- table 16
-    , Right $ toArray tableHuffR14  -- table 24
+    , Left $ toArray' tableHuffR05  -- table 7
+    , Left $ toArray' tableHuffR06  -- table 8
+    , Left $ toArray' tableHuffR07  -- table 9
+    , Left $ toArray' tableHuffR08  -- table 10
+    , Left $ toArray' tableHuffR09  -- table 11
+    , Left $ toArray' tableHuffR10  -- table 12
+    , Right $ toArray tableHuffR11 10 -- table 13
+    , Right $ toArray tableHuffR12 8  -- table 15
+    , Right $ toArray tableHuffR13 9  -- table 16
+    , Right $ toArray tableHuffR14 8  -- table 24
     ]
 
 -- The quadruple tables
 createTreesVWXY :: [MP3Huffman (Int,Int,Int,Int)]
 createTreesVWXY = [Left $ toArray' tableHuffRqa , Left $ toArray' tableHuffRqb]
 
-toArray :: BjrnTable a -> HuffDeep a
---toArray :: BjrnTable a -> [(Int,Either (Int,a) (HuffArray a))]
-toArray xs
-  = let (good,bad) = filterLength xs 10
-        lowhigh   = map (first (splitAt 10)) bad
+toArray :: BjrnTable a -> Int -> HuffDeep a
+toArray xs cut
+  = let (good,bad) = filterLength xs cut
+        lowhigh   = map (first (splitAt cut)) bad
         grouped   = groupBy (\a b -> (fst . fst) a == (fst . fst) b) (sortForGroups lowhigh)
         grouped'  = map (\as -> (fst $ fst $ head as, map (\((a,b),c) -> (b,c)) as)) grouped
         innerArrs = map (\(num,val) -> (toInt num , Right $ toArray' val)) grouped'
-        good'     = concatMap (\(a,b) -> map (\(a',l) -> (l,(a',b))) $ allLists 10 a) good
+        good'     = concatMap (\(a,b) -> map (\(a',l) -> (l,(a',b))) $ allLists cut a) good
         plainArrs = map (\(num,val) -> (toInt num , Left val)) good'
-    in (10, array (0,1023) $ innerArrs ++ plainArrs)
+    in (cut, array (0,2^cut - 1) $ innerArrs ++ plainArrs)
   where
     filterLength xs l = partition (\a -> length (fst a) <= l) xs
     
-
-{-
--- The "main" function, for transforming trees to arrays
-toArray :: BjrnTable a -> HuffDeep a
-toArray xs 
-        grouped'' = map (second (Right . toArray')) grouped'
-        allLists  = grouped'' ++ map (second Left) good
-    in (8, toArray' allLists)
-  where
-    mean ts = (sum $ map (length . fst) ts) `div` (length ts)
-    filterLength xs l = partition (\a -> length (fst a) <= l) xs
--}
-
 -- Creates an array of size 2^n where n is the length of the longest code word
 -- TODO: Give variables better names in the concatMap
 toArray' :: BjrnTable a -> HuffArray a
 toArray' xs = let xs'   = concatMap (\(a,b) -> map (\(a',l) -> (l,(a',b))) $ allLists n a) xs
                   xsInt = map (first toInt) xs'
-              in (n,array (0,n') $ sortBy (\(a,_) (b,_) -> compare a b) xsInt)
+              in (n,array (0,2^n - 1) $ sortBy (\(a,_) (b,_) -> compare a b) xsInt)
   where
     -- This is the length of the longest code word in the table
     n = foldl (\a entry -> max a (length $ fst entry)) 0 xs
-    n' = 2^n - 1
 
 -- Get all lists of length n that starts with xs
 allLists :: Int -> [Int] -> [(Int,[Int])]
