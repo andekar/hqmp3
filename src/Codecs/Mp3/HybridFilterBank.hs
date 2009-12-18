55a091427ab477017d0aeeb109cacdfb8b34f0ff
tobsan <tobsan@community.haskell.org>
2009-12-18 12:16:47 +0000

    hybrid filterbank cleanup

--- src/Codecs/Mp3/HybridFilterBank.hs
+++ src/Codecs/Mp3/HybridFilterBank.hs
@@ -36,26 +36,16 @@ import Codecs.Mp3.SynthesisFilterBank
 import Codecs.Mp3.Tables
 import Codecs.Mp3.Types
 
-{-
-mapBlock :: Int -> ([a] -> [b]) -> [a] -> [b]
-mapBlock blocksize func []  = []
-mapBlock blocksize func seq = 
-    let (block, rem) = splitAt blocksize seq
-    in func block ++ mapBlock blocksize func rem
--}
-
 mapBlock :: Int -> ([a] -> b) -> [a] -> [b]
 mapBlock blocksize func []  = []
 mapBlock blocksize func seq =
     let (block, rem) = splitAt blocksize seq
     in func block : mapBlock blocksize func rem
 
-
 -- 'windowWith' windows two signals. For readability, so we can do:
 -- signal' = signal `windowWith` win
 windowWith :: (Num a) => [a] -> [a] -> [a]
 windowWith = zipWith (*)
-
 
 -- 
 -- mp3IMDCT
@@ -66,12 +56,11 @@ windowWith = zipWith (*)
 --
 mp3IMDCT :: BlockFlag -> Int -> [Frequency] -> [Sample] -> ([Sample], [Sample])
 mp3IMDCT blockflag blocktype freq overlap =
-    let (samples, overlap') = 
-            case blockflag of
-                 LongBlocks  -> transf (doImdctLong blocktype) freq
-                 ShortBlocks -> transf (doImdctShort) freq
-                 MixedBlocks -> transf (doImdctLong 0)  (take 36 freq) <++>
-                                transf (doImdctShort) (drop 36 freq)
+    let (samples, overlap') = case blockflag of
+             LongBlocks  -> transf (doImdctLong blocktype) freq
+             ShortBlocks -> transf (doImdctShort) freq
+             MixedBlocks -> transf (doImdctLong 0) (take 36 freq) <++>
+                            transf (doImdctShort)  (drop 36 freq)
         samples' = zipWith (+) samples overlap
     in (samples', overlap')
     where
@@ -94,7 +83,6 @@ mp3IMDCT blockflag blocktype freq overlap =
 doImdctLong :: Int -> [Frequency] -> [Sample]
 doImdctLong blocktype f = imdct 18 f `windowWith` tableImdctWindow blocktype
 
-
 doImdctShort :: [Frequency] -> [Sample]
 doImdctShort f = overlap3 shorta shortb shortc
   where
@@ -102,17 +90,13 @@ doImdctShort f = overlap3 shorta shortb shortc
     shorta       = imdct 6 f1 `windowWith` tableImdctWindow 2
     shortb       = imdct 6 f2 `windowWith` tableImdctWindow 2
     shortc       = imdct 6 f3 `windowWith` tableImdctWindow 2
-    --shorta = imdct 6 [f!!0,f!!3,f!!6,f!!9,f!!12,f!!15] `windowWith` tableImdctWindow 2
-    --shortb = imdct 6 [f!!1,f!!4,f!!7,f!!10,f!!13,f!!16] `windowWith` tableImdctWindow 2
-    --shortc = imdct 6 [f!!2,f!!5,f!!8,f!!11,f!!14,f!!17] `windowWith` tableImdctWindow 2
-
+    
     overlap3 a b c = 
       p1 ++ (zipWith3 add3 (a ++ p2) (p1 ++ b ++ p1) (p2 ++ c)) ++ p1
       where
         add3 x y z = x+y+z
         p1         = [0,0,0, 0,0,0]
         p2         = [0,0,0, 0,0,0, 0,0,0, 0,0,0]
-
 
 splitAt2 :: Int -> [a] -> ([a], [a], [a])
 splitAt2 n xs = let (part1, part23) = splitAt n xs
@@ -123,19 +107,16 @@ splitAt2 n xs = let (part1, part23) = splitAt n xs
 (as, xs) <++> (bs, ys) = (as++bs, xs++ys)
 infixr 5 <++>
 
-
 -- 
 -- mp3AA
---
 -- Undo the encoders alias reduction.
---
 -- TODO: Rewrite this, clumsy and non-intuitive.
 --
 mp3AA :: BlockFlag -> Int -> [Frequency] -> [Frequency]
 mp3AA blockflag blocktype freq
   | blocktype == 2 && blockflag /= MixedBlocks   = freq
   | blocktype == 2 && blockflag == MixedBlocks   = 
-      (take 9 freq) ++ aaHelper (take  18 (drop 9 freq)) ++ (drop  27 freq)
+      (take 9 freq) ++ aaHelper (take 18 (drop 9 freq)) ++ (drop 27 freq)
   | otherwise                                    = 
       (take 9 freq) ++ aaHelper (take 558 (drop 9 freq)) ++ (drop 567 freq)
   where
@@ -156,25 +137,6 @@ mp3AA blockflag blocktype freq
       aaCoeff = [-0.6, -0.535, -0.33, -0.185, 
                  -0.095, -0.041, -0.0142, -0.0037]
 
-
-{-
-mp3AliasCancel :: BlockFlag -> [Frequency] -> [Frequency]
-mp3AliasCancel blockflag freq
-  | blockflag == ShortBlocks = freq
-  | blockflag == LongBlocks  = helper 31
-  | blockflag == MixedBlocks = helper 1
-  where
-    butterflies samples = let samplesa = zipWith (*) samples cs'
-                              samplesb = zipWith (*) (reverse samples) ca'
-                          in zipWith (+) samplesa samplesb
-   -- Coeffs.
-   cs' = reverse cs ++ cs
-   ca' = map (-) (reverse ca) ++ ca
-   cs = [1 / sqrt (1.0 + c**2) | c <- aaCoeff]
-   ca = [c / sqrt (1.0 + c**2) | c <- aaCoeff]
-   aaCoeff = [-0.6, -0.535, -0.33, -0.185, -0.095, -0.041, -0.0142, -0.0037]
--}
-
 -- 
 -- mp3FrequencyInvert
 --
@@ -184,19 +146,15 @@ mp3AliasCancel blockflag freq
 --
 mp3FrequencyInvert :: [Sample] -> [Sample]
 mp3FrequencyInvert = zipWith (*) pattern
-    where
-        pattern = cycle $ replicate 18 1 ++ take 18 (cycle [1,-1])
-
+    where pattern = cycle $ replicate 18 1 ++ take 18 (cycle [1,-1])
 
 -- Pads a list until it's length is n.
 -- padWith 5 0 [1,2,3] == [1,2,3,0,0]
 padWith :: Int -> a -> [a] -> [a]
-padWith n padding xs     = xs ++ replicate (n - length xs) padding
-
+padWith n padding xs = xs ++ replicate (n - length xs) padding
 
 -- 
 -- mp3HybridFilterBank
---
 -- Frequency domain to time domain.
 --
 mp3HybridFilterBank :: BlockFlag -> Int -> 
@@ -207,17 +165,16 @@ mp3HybridFilterBank bf bt (MP3HybridState simdct ssynthesis) input =
         aa                    = mp3AA    bf bt input'
         (samp, simdct')       = mp3IMDCT bf bt aa simdct
         samp'                 = mp3FrequencyInvert samp
-
-        --(ssynthesis', output) = (ssynthesis, take 18 samp) -- (See Driver.hs)
         (ssynthesis', output) = mp3SynthesisFilterBank ssynthesis samp'
-
     in (MP3HybridState simdct' ssynthesis', output)
-
 
 -- [Sample] = IMDCT output from previous granule, used for overlapping.
 -- MP3SynthState = State for the synthesis filterbank.
-data MP3HybridState = MP3HybridState [Sample] MP3SynthState deriving Show
+data MP3HybridState = MP3HybridState [Sample] MP3SynthState
 
 emptyMP3HybridState :: MP3HybridState
-emptyMP3HybridState = MP3HybridState (replicate 576 0.0) 
-                                     (MP3SynthState (replicate 1024 0.0))
+emptyMP3HybridState 
+    = MP3HybridState (replicate 576 0.0) 
+                     (MP3SynthState (listArray (0,1023) empty) empty) 
+  where empty = (replicate 1024 0.0)
+
